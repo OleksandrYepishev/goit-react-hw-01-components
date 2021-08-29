1907f8bd4a7a337577c9e194ab4a38b3519682d0
Oleksandr Yepishev <Alexandrdymka@gmail.com>
2021-08-29 18:07:26 +0300

    1st done

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import { Profile } from './Profile/Profile';
+import user from '../user.json';
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
